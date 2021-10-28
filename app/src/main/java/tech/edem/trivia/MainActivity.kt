7fa1c686d8f8c17f651ea325d417075bf77dc9d8
Boris <b_khramtsov@mail.ru>
2021-10-28 13:47:47 +0300

    Добавлены все классы

--- app/src/main/java/tech/edem/trivia/MainActivity.kt
+++ app/src/main/java/tech/edem/trivia/MainActivity.kt
@@ -2,10 +2,13 @@ package tech.edem.trivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import tech.edem.trivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
